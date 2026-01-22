972a37ed706460682e15a6e2405184b26027f56e
Scott Morris <scott.moris@gmail.com>
2026-01-21 22:52:33 -0500

    fix: repair desktop build for theme-utils
    - Gate `Mobile` error variant behind `#[cfg(mobile)]` in `error.rs`.
    - Fix mismatched types in `desktop.rs` init function (remove unused config generic).

--- plugins/theme-utils/src/desktop.rs
+++ plugins/theme-utils/src/desktop.rs
@@ -1,32 +1,31 @@
-use serde::de::DeserializeOwned;
 use tauri::{plugin::PluginApi, AppHandle, Runtime};
 
 use crate::models::{MaterialYouResponse, Palettes};
 
-pub fn init<R: Runtime, C: DeserializeOwned>(
-  api: PluginApi<R, C>,
-  _app: &AppHandle<R>,
+pub fn init<R: Runtime>(
+    api: PluginApi<R, ()>,
+    _app: &AppHandle<R>,
 ) -> crate::Result<ThemeUtils<R>> {
-  Ok(ThemeUtils(api))
+    Ok(ThemeUtils(api))
 }
 
 /// Access to the theme-utils APIs.
 pub struct ThemeUtils<R: Runtime>(PluginApi<R, ()>);
 
 impl<R: Runtime> ThemeUtils<R> {
-  pub fn get_material_you_colours(&self) -> crate::Result<MaterialYouResponse> {
-    // Desktop does not support Material You
-    println!("[ThemeUtils] Desktop platform detected, returning empty Material You response.");
-    Ok(MaterialYouResponse {
-        supported: false,
-        api_level: 0,
-        palettes: Palettes {
-            system_accent1: None,
-            system_accent2: None,
-            system_accent3: None,
-            system_neutral1: None,
-            system_neutral2: None,
-        }
-    })
-  }
+    pub fn get_material_you_colours(&self) -> crate::Result<MaterialYouResponse> {
+        // Desktop does not support Material You
+        println!("[ThemeUtils] Desktop platform detected, returning empty Material You response.");
+        Ok(MaterialYouResponse {
+            supported: false,
+            api_level: 0,
+            palettes: Palettes {
+                system_accent1: None,
+                system_accent2: None,
+                system_accent3: None,
+                system_neutral1: None,
+                system_neutral2: None,
+            },
+        })
+    }
 }
--- plugins/theme-utils/src/error.rs
+++ plugins/theme-utils/src/error.rs
@@ -3,21 +3,22 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum Error {
-  #[error(transparent)]
-  Io(#[from] std::io::Error),
-  #[error(transparent)]
-  Mobile(#[from] tauri::plugin::mobile::PluginInvokeError),
-  #[error("Plugin error: {0}")]
-  Plugin(String),
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+    #[cfg(mobile)]
+    #[error(transparent)]
+    Mobile(#[from] tauri::plugin::mobile::PluginInvokeError),
+    #[error("Plugin error: {0}")]
+    Plugin(String),
 }
 
 impl Serialize for Error {
-  fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
-  where
-    S: Serializer,
-  {
-    serializer.serialize_str(self.to_string().as_ref())
-  }
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(self.to_string().as_ref())
+    }
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
